9d1a9b724c29f1adb27f40fb68b1fd87efff0041
Иван Назолин <ivan@MacBook-Air-Ivan.local>
2025-08-23 11:43:23 +0300

    отправка сообщений

--- main.py
+++ main.py
@@ -2,8 +2,36 @@ import sqlite3
 import time
 import json
 import os
-
+from CONFIG import bdPath,bdName,logFile,trafficFile,BOT_TOKEN,CHAT_ID
 import requests
+
+active_sessions = {}
+
+
+def send_session_end_message(name, start_time, end_time, duration, up_mb, down_mb):
+    message = (
+        f"📡 <b>Сессия завершена</b>\n"
+        f"👤 Клиент: <b>{name}</b>\n"
+        f"🕒 Период: {start_time} – {end_time}\n"
+        f"⏱️ Длительность: {duration}\n"
+        f"📊 Трафик: {format_traffic((up_mb + down_mb))}\n"
+        f"⬆️ Upload: {format_traffic(up_mb)}\n"
+        f"⬇️ Download: {format_traffic(down_mb)}"
+    )
+    send_telegram_message(message)
+
+def send_telegram_message(message):
+    for CHAT_Id in CHAT_ID:
+        url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+        payload = {
+            "chat_id": CHAT_Id,
+            "text": message,
+            "parse_mode": "HTML"
+        }
+        try:
+            requests.post(url, data=payload)
+        except Exception as e:
+            print(f"Ошибка отправки в Telegram: {e}")
 
 def format_traffic(mb):
     return f"{mb:.2f} MB" if mb < 1024 else f"{mb/1024:.2f} GB"
